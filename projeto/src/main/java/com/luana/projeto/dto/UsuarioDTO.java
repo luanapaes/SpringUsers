9dc5a9a8e874620ce18b18c39411e9816b23d789
Luana Paes <114548504+luanapaes@users.noreply.github.com>
2024-03-14 18:26:36 -0300

    Update UsuarioDTO.java

--- projeto/src/main/java/com/luana/projeto/dto/UsuarioDTO.java
+++ projeto/src/main/java/com/luana/projeto/dto/UsuarioDTO.java
@@ -1,5 +1,57 @@
 package com.luana.projeto.dto;
 
+import org.springframework.beans.BeanUtils;
+
+import com.luana.projeto.entity.UsuarioEntity;
+
 public class UsuarioDTO {
+	
+
+	private long id;
+	private String nome;
+	private String login;
+	private String senha;
+	private String email;
+	
+	//lê um Entity e converte para DTO
+	public UsuarioDTO(UsuarioEntity usuario) {
+		BeanUtils.copyProperties(usuario, this);
+	}
+	
+	public UsuarioDTO() {
+		
+	}
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 }
