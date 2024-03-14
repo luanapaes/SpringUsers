ccff6ccd326128cd01f7cd82dcac741b842f3eee
Luana Paes <luanafranca598@gmail.com>
2024-03-14 17:36:52 -0300

    Criação dos atributos da classe UsuarioEntity
    -  Criação dos atributos da classe UsuarioEntity e dos Getters e Setters
    - Configurando o dialeto do PostgreSQL

--- projeto/src/main/java/com/luana/projeto/entity/UsuarioEntity.java
+++ projeto/src/main/java/com/luana/projeto/entity/UsuarioEntity.java
@@ -1,5 +1,71 @@
 package com.luana.projeto.entity;
 
-public class UsuarioEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "LP_USUARIO")
+public class UsuarioEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //gerará uma coluna do tipo SERIAL no Postgres
+	private long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false, unique = true)
+	private String login;
+	
+	@Column(nullable = false)
+	private String senha;
+	
+	@Column(nullable = false)
+	private String email;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
 }
