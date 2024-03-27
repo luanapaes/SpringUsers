391048c4c0191a46cef048a55af631a7b8f818c2
Luana Paes <luanafranca598@gmail.com>
2024-03-27 00:07:23 -0300

    Adicionado permissão do Cors

--- projeto/src/main/java/com/luana/projeto/controller/UsuarioController.java
+++ projeto/src/main/java/com/luana/projeto/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,27 +19,32 @@ import com.luana.projeto.service.UsuarioService;
 
 @RestController
 @RequestMapping(value = "/usuario")
+
 public class UsuarioController {
 	
 	@Autowired
 	private UsuarioService usuarioService;
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping
 	public List<UsuarioDTO> listarTodos(){
 		return usuarioService.listarTodos();
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PostMapping
 	public void inserir(@RequestBody UsuarioDTO usuario) {
 		usuarioService.inserirUsuario(usuario);
 	}
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@PutMapping
 	public UsuarioDTO alterar(@RequestBody UsuarioDTO usuario) {
 		return usuarioService.alterarUsuario(usuario);
 	}
 	
 	//exemplo - http://endereco-front/usuario/3
+	@CrossOrigin(origins = "http://localhost:4200")
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Void> excluir(@PathVariable("id") Long id){
 		usuarioService.excluirUsuario(id);
